d507ccf7010fee9a6bdad0e0888fc0c5eefcf735
Vikas Swami <vikasbswami@gmail.com>
2017-07-19 17:56:51 +0530

    Working bot

--- bot.js
+++ bot.js
@@ -1,9 +1,35 @@
-console.log('The bot is starting...');
 
 var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-T.post('statuses/update', { status: 'It works!' }, function(err, data, response) {
-  console.log(data)
-})
+var params = {
+  q: 'india OR rivers',
+  geocode: '20.5937,78.9629,1700km',
+  lang: 'en',
+  count: 1
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData (err, data, response) {
+  var tweets = data.statuses;
+  var rTweet = randIndex(tweets)
+  	if(typeof rTweet != 'undefined')
+  	{
+  		var targetId = rTweet.user.id_str;
+      var targetScreenName = rTweet.user.screen_name;
+
+      var postParams = {
+        status: '@'+targetScreenName+' Rally for Rivers (#RallyForRivers) - Campaign Launch by Sadhguru https://youtu.be/k9dkhESbi5A via @YouTube'
+      }
+
+  		T.post('statuses/update', postParams, function (err, data, response) {
+      });
+  	}
+}
+
+function randIndex (arr) {
+  var index = Math.floor(arr.length*Math.random());
+  return arr[index];
+};
