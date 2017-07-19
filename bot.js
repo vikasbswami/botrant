e4674553749e695bd112b5a615e924c3a00b93ba
Vikas Swami <vikasbswami@gmail.com>
2017-07-19 18:29:51 +0530

    setInterval done

--- bot.js
+++ bot.js
@@ -3,14 +3,19 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-var params = {
+var getParams = {
   q: 'india OR rivers',
   geocode: '20.5937,78.9629,1700km',
   lang: 'en',
   count: 1
 }
 
-T.get('search/tweets', params, gotData);
+function tweetIt() {
+  T.get('search/tweets', getParams, gotData);
+}
+
+tweetIt();
+setInterval(tweetIt, 1000*60*5);
 
 function gotData (err, data, response) {
   var tweets = data.statuses;
@@ -24,8 +29,11 @@ function gotData (err, data, response) {
         status: '@'+targetScreenName+' Rally for Rivers (#RallyForRivers) - Campaign Launch by Sadhguru https://youtu.be/k9dkhESbi5A via @YouTube'
       }
 
-  		T.post('statuses/update', postParams, function (err, data, response) {
-      });
+  		T.post('statuses/update', postParams, postedData);
+
+      function postedData (err, data, response) {
+        console.log(data);
+      }
   	}
 }
 
